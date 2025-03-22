6b580e56f08b877940d4c17a4981f6653ced1c87
ShofiSo <kelly.rabanales@gmail.com>
2025-03-21 20:50:47 -0600

    Quinto Commit
    Cambie las clases y agregué unas cuantas modificaciones porque el código no corría

--- PaqueteCaramelos.java
+++ PaqueteCaramelos.java
@@ -2,36 +2,44 @@ import java.util.Stack;
 
 public class PaqueteCaramelos {
 
-    private Stack<String> pilaCaramelos; 
+    private Stack<String> pilaCaramelos;  
 
     public PaqueteCaramelos(String[] caramelos) {
         pilaCaramelos = new Stack<>();
         for (String caramelo : caramelos) {
-            pilaCaramelos.push(caramelo); 
+            pilaCaramelos.push(caramelo);  
         }
     }
+
     public void procesarCaramelos() {
-        Stack<String> caramelosDevueltos = new Stack<>(); 
+        Stack<String> caramelosDevueltos = new Stack<>();  // Pila para los caramelos no amarillos
+
+        System.out.println("Iniciando proceso...");
 
         while (!pilaCaramelos.isEmpty()) {
-            String caramelo = pilaCaramelos.pop();  
+            String caramelo = pilaCaramelos.pop();  // Sacamos un caramelo de la pila
+
+            System.out.println("Sacando caramelo: " + caramelo);
 
             if ("amarillo".equals(caramelo)) {
-                System.out.println("Comiendo caramelo " + caramelo);
+                System.out.println("¡Comiendo caramelo " + caramelo + "!");
             } else {
-                caramelosDevueltos.push(caramelo);  
+                System.out.println("Devolviendo caramelo " + caramelo + " al paquete.");
+                caramelosDevueltos.push(caramelo);  // Si no es amarillo, lo devolvemos a la pila de caramelos
             }
         }
         while (!caramelosDevueltos.isEmpty()) {
             String caramelo = caramelosDevueltos.pop();
-            pilaCaramelos.push(caramelo);  // Acá devolvemos los caramelos al paquete
+            pilaCaramelos.push(caramelo);  // Los devolvemos al paquete
         }
 
-        System.out.println("Caramelos restantes en el paquete: " + pilaCaramelos);
+        System.out.println("\nCaramelos restantes en el paquete: " + pilaCaramelos);
     }
+
     public static void main(String[] args) {
-        String[] caramelos = {"Rojo", "Amarillo", "Verde", "Amarillo", "Azul", "amarillo", "rojo"};
+        String[] caramelos = {"rojo", "amarillo", "verde", "amarillo", "azul", "amarillo", "rojo"};
         PaqueteCaramelos paquete = new PaqueteCaramelos(caramelos);
+
 
         paquete.procesarCaramelos();
     }
